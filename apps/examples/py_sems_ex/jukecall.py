b126b455e2f8432ca5a7e889025c25039b0f0e8e
Michael Prokop <mprokop@sipwise.com>
2022-12-29 10:14:27 +0100

    MT#55831 Build and include SBC tools + examples, port to py3k
    Otherwise we're missing plenty of audio files in /usr/lib/ngcp-sems/audio/,
    several plugins in /usr/lib/ngcp-sems/plug-in/, as well as a bunch of binaries:
    
    * /usr/sbin/ngcp-sems-get-callproperties
    * /usr/sbin/ngcp-sems-list-active-calls
    * /usr/sbin/ngcp-sems-list-calls
    * /usr/sbin/ngcp-sems-list-finished-calls
    * /usr/sbin/ngcp-sems-sbc-get-activeprofile
    * /usr/sbin/ngcp-sems-sbc-get-regex-map-names
    * /usr/sbin/ngcp-sems-sbc-list-profiles
    * /usr/sbin/ngcp-sems-sbc-load-callcontrol-modules
    * /usr/sbin/ngcp-sems-sbc-load-profile
    * /usr/sbin/ngcp-sems-sbc-reload-profile
    * /usr/sbin/ngcp-sems-sbc-reload-profiles
    * /usr/sbin/ngcp-sems-sbc-set-activeprofile
    * /usr/sbin/ngcp-sems-sbc-set-regex-map
    * /usr/sbin/ngcp-sems-sbc-teardown-call
    * /usr/sbin/ngcp-sems-webconference-addparticipant
    * /usr/sbin/ngcp-sems-webconference-roomcreate
    * /usr/sbin/ngcp-sems-webconference-roominfo
    
    This fixes a regression introduced in commit 1619876f76d (TT#101059).
    
    Ported the python2 scripts we ship with our Debian package to python3
    (AKA py3k), fixed inconsistent use of tabs and spaces in indentation
    and indention in several files:
    
    * apps/sbc/tools/sems-sbc-*
    * apps/examples/db_announce/announcement.py
    * apps/examples/py_sems_ex/*py
    
    We're also not installing the *.pyc files (see
    apps/ivr/Makefile.ivr_application +
    apps/py_sems/Makefile.py_sems_application), they don't exist in py3k
    builds and it wouldn't make any sense to ship them in Debian packages
    anyway.
    (real ticket number: TT#105412)
    
    Change-Id: I44f0f76b4577501eb31fe814781f47087cfd16dd

--- apps/examples/py_sems_ex/jukecall.py
+++ apps/examples/py_sems_ex/jukecall.py
@@ -1,110 +1,123 @@
-import base64,time,os,sip
+import base64, time, os, sip
 
 from py_sems_log import *
 from py_sems import *
 from py_sems_lib import *
 
+
 class MyB2ABEvent(PySemsB2ABEvent):
-	def __init__(self, id):
-		PySemsB2ABEvent.__init__(self,id)
+    def __init__(self, id):
+        PySemsB2ABEvent.__init__(self, id)
+
 
 class MyCalleeSession(PySemsB2ABCalleeDialog):
-	def __init__(self, tag):
-		debug("**** __init callee __ ****")
-		AmB2ABCalleeSession.__init__(self, tag)
-		self.ann=None
-		#debug("**** tag = " + tag);
+    def __init__(self, tag):
+        debug("**** __init callee __ ****")
+        AmB2ABCalleeSession.__init__(self, tag)
+        self.ann = None
+        # debug("**** tag = " + tag);
 
-	def onPyB2ABEvent(self, ev):
-		debug("***************************** callee PyB2ABEvent  ************************")
-		if isinstance(ev, MyB2ABEvent):
-			self.ann = AmAudioFile()
-			self.ann.open("/tmp/test.wav")
-			self.setOutput(self.ann)
-			return
+    def onPyB2ABEvent(self, ev):
+        debug(
+            "***************************** callee PyB2ABEvent  ************************"
+        )
+        if isinstance(ev, MyB2ABEvent):
+            self.ann = AmAudioFile()
+            self.ann.open("/tmp/test.wav")
+            self.setOutput(self.ann)
+            return
 
-			
+
 class PySemsScript(PySemsB2ABDialog):
+    def __init__(self):
 
-	def __init__(self):
-		
-		debug("***** __init__ *******")
-		PySemsB2ABDialog.__init__(self)
-		self.initial_user = None
-		self.initial_domain = None
-		self.initial_fromuri = None
-		self.ann = None
-		sip.settracemask(0xFFFF)
+        debug("***** __init__ *******")
+        PySemsB2ABDialog.__init__(self)
+        self.initial_user = None
+        self.initial_domain = None
+        self.initial_fromuri = None
+        self.ann = None
+        sip.settracemask(0xFFFF)
 
-	def onInvite(self, req):
-		if len(req.user) < 2:
-			self.dlg.reply(req,500,"Need a number to dial","","","")
-			self.setStopped()
-			return
+    def onInvite(self, req):
+        if len(req.user) < 2:
+            self.dlg.reply(req, 500, "Need a number to dial", "", "", "")
+            self.setStopped()
+            return
 
-		ann_file = self.getAnnounceFile(req)
-		self.ann = AmAudioFile()
-		try:
-			self.ann.open(ann_file)
-		except:
-			self.dlg.reply(req,500,"File not found","","","")
-			self.ann = None
-			self.setStopped()
-			raise
-			
-		PySemsB2ABDialog.onInvite(self,req)
+        ann_file = self.getAnnounceFile(req)
+        self.ann = AmAudioFile()
+        try:
+            self.ann.open(ann_file)
+        except:
+            self.dlg.reply(req, 500, "File not found", "", "", "")
+            self.ann = None
+            self.setStopped()
+            raise
 
-  	def onSessionStart(self,req):
-		self.setOutput(self.ann)
-		self.initial_user = req.user
-		self.initial_domain = req.domain
-		self.initial_fromuri = req.from_uri
+        PySemsB2ABDialog.onInvite(self, req)
 
-	def getAnnounceFile(self,req):
+    def onSessionStart(self, req):
+        self.setOutput(self.ann)
+        self.initial_user = req.user
+        self.initial_domain = req.domain
+        self.initial_fromuri = req.from_uri
 
-		announce_file = config["announce_path"] + req.domain + "/" + get_header_param(req.r_uri, "play") + ".wav"
+    def getAnnounceFile(self, req):
 
-		debug("trying '%s'",announce_file)
-		if os.path.exists(announce_file):
-			return announce_file
+        announce_file = (
+            config["announce_path"]
+            + req.domain
+            + "/"
+            + get_header_param(req.r_uri, "play")
+            + ".wav"
+        )
 
-		announce_file = config["announce_path"] + req.user + ".wav"
-		debug("trying '%s'",announce_file)
-		if os.path.exists(announce_file):
-			return announce_file
+        debug("trying '%s'", announce_file)
+        if os.path.exists(announce_file):
+            return announce_file
 
-		announce_file = config["announce_path"] + config["announce_file"]
-		debug("using default '%s'",announce_file)
-		return announce_file
+        announce_file = config["announce_path"] + req.user + ".wav"
+        debug("trying '%s'", announce_file)
+        if os.path.exists(announce_file):
+            return announce_file
 
+        announce_file = config["announce_path"] + config["announce_file"]
+        debug("using default '%s'", announce_file)
+        return announce_file
 
-	def process(self,ev):
+    def process(self, ev):
 
-		debug("*********** PySemsScript.process **************")
-		if isinstance(ev,AmAudioEvent):
-		  if ev.event_id == AmAudioEvent.cleared:
-			debug("AmAudioEvent.cleared")
-			to = self.initial_user[1:len(self.initial_user)] + \
-				"@" + self.initial_domain
-			debug("to is " + to)
-			debug("from is "+ self.initial_fromuri)
-			self.connectCallee("<sip:"+to+">", "sip:"+to, \
-				self.initial_fromuri, self.initial_fromuri)
-			debug("connectcallee ok")
-			return
-		
-		PySemsB2ABDialog.process(self,ev);
-		return
+        debug("*********** PySemsScript.process **************")
+        if isinstance(ev, AmAudioEvent):
+            if ev.event_id == AmAudioEvent.cleared:
+                debug("AmAudioEvent.cleared")
+                to = (
+                    self.initial_user[1 : len(self.initial_user)]
+                    + "@"
+                    + self.initial_domain
+                )
+                debug("to is " + to)
+                debug("from is " + self.initial_fromuri)
+                self.connectCallee(
+                    "<sip:" + to + ">",
+                    "sip:" + to,
+                    self.initial_fromuri,
+                    self.initial_fromuri,
+                )
+                debug("connectcallee ok")
+                return
 
-	def createCalleeSession(self):
-		print self.dlg.local_tag
-		cs = MyCalleeSession(self.dlg.local_tag)
-		print cs
-		return cs
+        PySemsB2ABDialog.process(self, ev)
+        return
 
-	def onDtmf(self, event, dur):
-		debug("************ onDTMF: ********* " + str(event) + "," + str(dur))
-		ev  = MyB2ABEvent(15)
-		self.relayEvent(ev)
-	
-			
+    def createCalleeSession(self):
+        print(self.dlg.local_tag)
+        cs = MyCalleeSession(self.dlg.local_tag)
+        print(cs)
+        return cs
+
+    def onDtmf(self, event, dur):
+        debug("************ onDTMF: ********* " + str(event) + "," + str(dur))
+        ev = MyB2ABEvent(15)
+        self.relayEvent(ev)
