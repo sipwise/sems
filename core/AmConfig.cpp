ddde01c0a36745db75fe6d5d9a55813cdaa1a275
Donat Zenichev <dzenichev@sipwise.com>
2023-05-11 11:10:26 +0200

    MT#57402 warning: specified bound 16 equals destination size
    Fix for the following warning:
    
       In function 'char* strncpy(char*, const char*, size_t)',
           inlined from 'bool fillSysIntfList()' at AmConfig.cpp:1014:14,
           inlined from 'static int AmConfig::finalizeIPConfig()' at AmConfig.cpp:1137:18:
           /usr/include/x86_64-linux-gnu/bits/string_fortified.h:106:34: warning: 'char* __builtin_strncpy(char*, const char*, long unsigned int)'
           specified bound 16 equals destination size [-Wstringop-truncation]
           |   return __builtin___strncpy_chk (__dest, __src, __len, __bos (__dest));
    
    Change-Id: If1610c7ac408ce596b02d95a43558475b87d061b

--- core/AmConfig.cpp
+++ core/AmConfig.cpp
@@ -1011,7 +1011,8 @@ static bool fillSysIntfList()
       intf_it->flags = p_if->ifa_flags;
 
       struct ifreq ifr;
-      strncpy(ifr.ifr_name,p_if->ifa_name,IFNAMSIZ);
+      strncpy(ifr.ifr_name, p_if->ifa_name, sizeof(ifr.ifr_name) - 1);
+      ifr.ifr_name[sizeof(ifr.ifr_name) - 1] = '\0';
 
       if (ioctl(fd, SIOCGIFMTU, &ifr) < 0 ) {
 	ERROR("ioctl: %s",strerror(errno));
