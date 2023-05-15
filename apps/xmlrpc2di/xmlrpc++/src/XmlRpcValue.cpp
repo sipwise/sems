42b9ad135f36f2edf0d2f2c0941691e69466b9ad
Donat Zenichev <dzenichev@sipwise.com>
2023-05-15 13:25:08 +0200

    MT#57436 directive output may be truncated
    We should provide a proper formation of the timestamp
    in the `timeToXml()`. Just unify that in the manner
    it's done in the rest of places.
    
    It will fix then this:
    
         src/XmlRpcValue.cpp: In member function 'std::string XmlRpc::XmlRpcValue::timeToXml() const':
         src/XmlRpcValue.cpp:404:53: warning: '%02d' directive output may be truncated writing between 2 and 11 bytes into a region of size between 0 and 7 [-Wformat-truncation=]
           404 |     snprintf(buf, sizeof(buf)-1, "%04d%02d%02dT%02d:%02d:%02d",
               |                                                     ^~~~
         In file included from /usr/include/stdio.h:867,
                          from /usr/include/c++/10/cstdio:42,
                          from /usr/include/c++/10/ext/string_conversions.h:43,
                          from /usr/include/c++/10/bits/basic_string.h:6545,
                          from /usr/include/c++/10/string:55,
                          from src/XmlRpcValue.h:14,
                          from src/XmlRpcValue.cpp:2:
         /usr/include/x86_64-linux-gnu/bits/stdio2.h:67:35: note: '__builtin___snprintf_chk' output between 18 and 70 bytes into a destination of size 19
            67 |   return __builtin___snprintf_chk (__s, __n, __USE_FORTIFY_LEVEL - 1,
               |          ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
            68 |        __bos (__s), __fmt, __va_arg_pack ());
               |        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Change-Id: Iadfed6c14e1bdb0c8ed0b8b36b996ed08b129c42

--- apps/xmlrpc2di/xmlrpc++/src/XmlRpcValue.cpp
+++ apps/xmlrpc2di/xmlrpc++/src/XmlRpcValue.cpp
@@ -401,8 +401,8 @@ namespace XmlRpc {
   {
     struct tm* t = _value.asTime;
     char buf[20];
-    snprintf(buf, sizeof(buf)-1, "%04d%02d%02dT%02d:%02d:%02d", 
-      1900+t->tm_year,t->tm_mon,t->tm_mday,t->tm_hour,t->tm_min,t->tm_sec);
+    snprintf(buf, sizeof(buf)-1, "%4d%02d%02dT%02d:%02d:%02d",
+             t->tm_year, t->tm_mon, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
     buf[sizeof(buf)-1] = 0;
 
     std::string xml = VALUE_TAG;
