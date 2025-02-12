c0e7b0ccbac1e54de3450c76164e6d100a3f7653
Donat Zenichev <dzenichev@sipwise.com>
2025-02-12 16:01:04 +0100

    MT#57728 Introduce Read the Docs
    This is a first commit introducing general readthedocs wiki
    structure.
    
    It introduced the following main chapters:
    - Overview (is a symlink to the README.md), the readthedocs
      parses the markdown from here and creates the overview page
    
    Mainly files required for RTD building.
    
    Change-Id: Iad5626da6cff991f272cb0b2260888bba568f622

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from datetime import date
+
+project = 'sems'
+copyright = str(date.today().year) + ', Sipwise'
+author = 'Sipwise'
+release = 'master'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser",
+              "sphinx_rtd_theme"]
+myst_heading_anchors = 3
+
+templates_path = ['_templates']
+exclude_patterns = ['_build']
+
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+htmlhelp_basename = 'semsdoc'
---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+.. _SEMS: https://github.com/sipwise/sems
+.. _rtpengine: https://github.com/sipwise/rtpengine
+.. _Sipwise: https://www.sipwise.com
+.. _Kamailio: https://www.kamailio.org/
+
+Welcome to the SEMS Project Documentation
+==============================================
+
+.. toctree::
+   :maxdepth: 4
+
+   Overview <overview>
