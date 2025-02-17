b3295502e817d53946fbd07125e4f1f8ea5651be
Donat Zenichev <dzenichev@sipwise.com>
2025-02-17 17:58:55 +0100

    MT#59962 trixie: sw_vsc, Fix type compilation warnings
    Fixes things like:
    
    16: 52:56   137 |   _LOG(L_ERR, error_category " " fmt, ##args)
    16: 52:56 ../../core/log.h:121:45: note: in definition of macro '_LOG'
    16: 52:56   121 |       int n_ = snprintf(msg_, sizeof(msg_), fmt, ##args);               \
    16: 52:56       |                                             ^~~
    16: 52:59 ../../core/log.h:163:29: note: in expansion of macro 'CAT_ERROR'
    16: 53:00   163 | #define ERROR(fmt, args...) CAT_ERROR(ERROR_CATEGORY_EGENERAL, fmt, ##args)
    16: 53:00       |                             ^~~~~~~~~
    16: 53:00 SW_Vsc.cpp:304:9: note: in expansion of macro 'ERROR'
    16: 53:00   304 |         ERROR("Found invalid number of id entries for attribute '%s': %lu",
    16: 53:00       |         ^~~~~
    16: 53:00 SW_Vsc.cpp:304:73: note: format string is defined here
    16: 53:00   304 |         ERROR("Found invalid number of id entries for attribute '%s': %lu",
    16: 53:00       |                                                                       ~~^
    16: 53:00       |                                                                         |
    16: 53:00       |                                                                         long unsigned int
    16: 53:00       |                                                                       %llu
    Change-Id: I750c7992012fa926a992b5fcc41da27f4a65a55e

--- apps/sw_vsc/SW_Vsc.cpp
+++ apps/sw_vsc/SW_Vsc.cpp
@@ -301,7 +301,7 @@ u_int64_t SW_VscDialog::getAttributeId(MYSQL *my_handler, const char *attribute)
     res = mysql_store_result(my_handler);
     if (mysql_num_rows(res) != 1)
     {
-        ERROR("Found invalid number of id entries for attribute '%s': %lu",
+        ERROR("Found invalid number of id entries for attribute '%s': %llu",
               attribute, mysql_num_rows(res));
         return 0;
     }
@@ -381,7 +381,7 @@ u_int64_t SW_VscDialog::getSubscriberId(MYSQL *my_handler, const char *uuid,
     res = mysql_store_result(my_handler);
     if (mysql_num_rows(res) != 1)
     {
-        ERROR("Found invalid number of id entries for uuid '%s': %lu",
+        ERROR("Found invalid number of id entries for uuid '%s': %llu",
               uuid , mysql_num_rows(res));
         return 0;
     }
@@ -434,7 +434,7 @@ u_int64_t SW_VscDialog::getPreference(MYSQL *my_handler, u_int64_t subscriberId,
     }
     if (mysql_num_rows(res) != 1)
     {
-        ERROR("Found invalid number of id entries for subscriber id '%llu' and attribute id '%llu': %lu",
+        ERROR("Found invalid number of id entries for subscriber id '%llu' and attribute id '%llu': %llu",
               (unsigned long long int)subscriberId,
               (unsigned long long int)attributeId, mysql_num_rows(res));
         mysql_free_result(res);
